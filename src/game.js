8340e83ff1aef378a8cb7458dab69b84b3af90d0
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-20 13:09:49 +0100

    game class initialeres added

--- src/game.js
+++ src/game.js
@@ -2,7 +2,10 @@
 // the actions between players, the board, and ships.
 class Game {
   constructor() {
-    // Initialize the game
+    // Initialize the game state
+    this.players = []; // An array to hold the players
+    this.currentPlayerIndex = 0; // Index of the current player in the players array
+    this.gameOver = false; // Flag to track if the game is over
   }
 
   // Game loop and logic
