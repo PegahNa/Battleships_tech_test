0751fd77f4d0179e20f4fcb29ea9e74603c69c64
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-20 16:31:07 +0100

    board.test added

--- tests/unit/board.test.js
+++ tests/unit/board.test.js
@@ -1,1 +1,20 @@
-const Board = require("../../src/board");
+// Import the Board class from the appropriate location
+import Board from "../../src/board";
+
+describe("Board Class Unit Tests", () => {
+  // Test for board initialization
+  test("Should initialize the board with the correct size and empty cells", () => {
+    // Create an instance of the Board class
+    const boardInstance = new Board();
+
+    // Assertions to check the board size and cells
+    expect(boardInstance.size).toBe(10); // Assuming the board size is 10x10
+    expect(boardInstance.grid.length).toBe(10);
+    expect(boardInstance.grid.every((row) => row.length === 10)).toBe(true);
+
+    // All cells should be initialized as null (empty)
+    expect(boardInstance.grid.flat().every((cell) => cell === null)).toBe(true);
+  });
+
+  // Add more unit tests for the Board class as needed
+});
