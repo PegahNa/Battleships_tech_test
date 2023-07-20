3b6a87b0c5b584ac1cdc9dd3abe2763918ae7312
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-20 15:57:06 +0100

    initialses added to ship class

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,10 @@
 // The Ship class represents a ship in the game and communicates
 // with the Board class to validate placements and update the board state.
 class Ship {
-  constructor() {
-    // Ship properties and initialization
+  constructor(name, size) {
+    this.name = name;
+    this.size = size;
+    this.hits = new Array(size).fill(false);
   }
 
   // Methods related to ship status and behavior
