4781b4a9d22b400f20f74a6b6299659bc21e9737
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-20 13:33:41 +0100

    start and gameLoop method added to the game class

--- src/game.js
+++ src/game.js
@@ -12,11 +12,49 @@ class Game {
   addPlayer(player) {
     if (this.players.length < 2) {
       this.players.push(player);
-      console.log("${player.name} has joined the game! ");
+      console.log(`${player.name} has joined the game! `);
     } else {
       console.log("maximum number of players reached!");
     }
   }
+
+  start() {
+    if (this.players.length < 2) {
+      console.log("Cannot start the game. Need at least 2 players.");
+      return;
+    }
+
+    console.log("Game started!");
+
+    // Game setup and initialization can go here
+
+    this.gameLoop();
+  }
+
+  // Method to handle the game loop and player turns
+  gameLoop() {
+    while (!this.gameOver) {
+      const currentPlayer = this.players[this.currentPlayerIndex];
+      console.log(`${currentPlayer.name}'s turn: `);
+      // Implement logic for the player's turn here
+
+      // After a turn, check for game over condition
+      this.checkGameOver();
+
+      // Switch to the next player's turn
+      this.currentPlayerIndex =
+        (this.currentPlayerIndex + 1) % this.players.length;
+    }
+  }
+
+  // Method to check for game over condition
+  checkGameOver() {
+    // Implement logic to check if the game is over
+    // For example, check if all ships of any player are sunk
+    // If the game is over, set this.gameOver to true
+  }
+
+  // Other methods of the Game class will be implemented here
 }
 
 // Main entry point
