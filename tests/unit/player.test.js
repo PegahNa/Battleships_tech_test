610eac7f3ad677783f0b3dd89235f859e956722a
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-20 16:32:35 +0100

    player.test added

--- tests/unit/player.test.js
+++ tests/unit/player.test.js
@@ -1,1 +1,18 @@
-const Player = require("../../src/player");
+// Import the Player class from the appropriate location
+import Player from "../../src/player";
+
+describe("Player Class Unit Tests", () => {
+  // Test for player initialization
+  test("Should initialize the player with the correct name and an empty list of ships", () => {
+    const playerName = "Player 1";
+
+    // Create an instance of the Player class
+    const playerInstance = new Player(playerName);
+
+    // Assertions to check player name and ships
+    expect(playerInstance.name).toBe(playerName);
+    expect(playerInstance.ships.length).toBe(0);
+  });
+
+  // Add more unit tests for the Player class as needed
+});
