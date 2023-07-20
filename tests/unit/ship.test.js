fc1e87158a537a22e80e394ada7bbfdae7edf12e
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-20 16:33:53 +0100

    ship test added

--- tests/unit/ship.test.js
+++ tests/unit/ship.test.js
@@ -1,1 +1,19 @@
-const Ship = require("../../src/ship");
+// Import the Ship class from the appropriate location
+import Ship from "../../src/ship";
+
+describe("Ship Class Unit Tests", () => {
+  // Test for ship initialization
+  test("Should initialize the ship with the correct length and hit status", () => {
+    const shipLength = 4;
+
+    // Create an instance of the Ship class
+    const shipInstance = new Ship(shipLength);
+
+    // Assertions to check ship length and hit status
+    expect(shipInstance.length).toBe(shipLength);
+    expect(shipInstance.hits.length).toBe(shipLength);
+    expect(shipInstance.hits.every((hit) => hit === false)).toBe(true);
+  });
+
+  // Add more unit tests for the Ship class as needed
+});
