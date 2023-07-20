81509a3b836d48e0e8ece52e1891c73beea83857
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-20 15:10:23 +0100

    placeShip method added to player class

--- src/player.js
+++ src/player.js
@@ -7,7 +7,19 @@ class Player {
     this.ships = [];
   }
 
-  // Methods related to player actions, turns, and score tracking
+  // Method to place a ship on the player's board
+  placeShip(ship, position, direction) {
+    // Check if the placement is valid and the ship doesn't overlap with other ships
+    if (this.board.isValidPlacement(ship, position, direction)) {
+      this.board.placeShip(ship, position, direction);
+      this.ships.push(ship);
+      console.log(
+        `${this.name} placed a ${ship.name} at ${position} facing ${direction}.`
+      );
+    } else {
+      console.log("Invalid ship placement. Try again.");
+    }
+  }
 }
 
 module.exports = Player;
